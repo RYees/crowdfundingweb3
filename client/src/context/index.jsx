a316931a431a53c1f16fd3065ceb6b56095cc92f
Rehmet Yeshanew <ryfgmz87@gmail.com>
2023-01-19 22:11:48 -0500

    contract context

--- client/src/context/index.jsx
+++ client/src/context/index.jsx
@@ -7,7 +7,7 @@ import { EditionMetadataWithOwnerOutputSchema } from '@thirdweb-dev/sdk';
 const StateContext = createContext();
 
 export const StateContextProvider = ({ children }) => {
-  const { contract } = useContract('0xf59A1f8251864e1c5a6bD64020e3569be27e6AA9');
+  const { contract } = useContract('0x68553a3F3dFB17957A3b641CBf16AaE23dF96903');
   const { mutateAsync: createCampaign } = useContractWrite(contract, 'createCampaign');
 
   const address = useAddress();
