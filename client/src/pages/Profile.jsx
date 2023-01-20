d24178ad04a4c0b8e5b063e98c5d2d3aa7c1cf8a
Rehmet Yeshanew <ryfgmz87@gmail.com>
2023-01-20 17:47:14 -0500

    profile component

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -4,30 +4,29 @@ import { DisplayCampaigns } from '../components';
 import { useStateContext } from '../context'
 
 const Profile = () => {
-  <div>Profile</div>
-  // const [isLoading, setIsLoading] = useState(false);
-  // const [campaigns, setCampaigns] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [campaigns, setCampaigns] = useState([]);
 
-  // const { address, contract, getUserCampaigns } = useStateContext();
+  const { address, contract, getUserCampaigns } = useStateContext();
 
-  // const fetchCampaigns = async () => {
-  //   setIsLoading(true);
-  //   const data = await getUserCampaigns();
-  //   setCampaigns(data);
-  //   setIsLoading(false);
-  // }
+  const fetchCampaigns = async () => {
+    setIsLoading(true);
+    const data = await getUserCampaigns();
+    setCampaigns(data);
+    setIsLoading(false);
+  }
 
-  // useEffect(() => {
-  //   if(contract) fetchCampaigns();
-  // }, [address, contract]);
+  useEffect(() => {
+    if(contract) fetchCampaigns();
+  }, [address, contract]);
 
-  // return (
-  //   <DisplayCampaigns 
-  //     title="All Campaigns"
-  //     isLoading={isLoading}
-  //     campaigns={campaigns}
-  //   />
-  // )
+  return (
+    <DisplayCampaigns 
+      title="All Campaigns"
+      isLoading={isLoading}
+      campaigns={campaigns}
+    />
+  )
 }
 
 export default Profile
